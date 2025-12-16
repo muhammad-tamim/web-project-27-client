1c9c601d491348f50d5c2cc6075aa481096c91b7
muhammad-tamim <contact2tamim@gmail.com>
2025-12-16 09:27:27 +0600

    chore: add redirects file for netlify

--- src/features/public/components/Navbar.tsx
+++ src/features/public/components/Navbar.tsx
@@ -1,9 +1,16 @@
+import logo from "@/assets/logos/logo.png"
+import { Link } from "react-router";
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="max-w-[1200px] mx-auto">
+            <div>
+                <Link to={'/'}><img src={logo} className="mx-auto py-7 max-w-[150px]" alt="logo" /></Link>
+            </div>
+            <div>
+
+            </div>
+        </nav>
     );
 };
 
